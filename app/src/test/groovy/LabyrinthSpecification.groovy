a7629012731301aeb0bdb60f14bbbea2698c6ced
Martina Lucan <martina.lucan@adcubum.com>
2021-04-13 08:50:06 +0200

    tests refinement for Labyrinth

--- app/src/test/groovy/LabyrinthSpecification.groovy
+++ app/src/test/groovy/LabyrinthSpecification.groovy
@@ -1,6 +1,7 @@
 import model.Labyrinth
 import model.Position
 import spock.lang.Specification
+import spock.lang.Unroll
 import util.FileUtils
 
 class LabyrinthSpecification extends Specification {
@@ -26,7 +27,8 @@ class LabyrinthSpecification extends Specification {
         charOnCurrentPosition == '@'
     }
 
-    def "test getCharOnPosition_providedNoNEmptyList_charOnCurrentPositionIs-"() {
+    @Unroll
+    def "test getCharOnPosition_providedNoNEmptyList_charOnCurrentPositionIs#expectedChar"() {
         given:
         List<String> lines = FileUtils.readFile("ascii_path_1.txt")
         Labyrinth labyrinth = new Labyrinth(lines);
@@ -36,17 +38,149 @@ class LabyrinthSpecification extends Specification {
 
         then:
         charOnCurrentPosition == '-'
+
+        where:
+        sourceRowIndex | sourceColumnIndex || expectedChar
+        0              | 2                 || '-'
+        0              | 5                 || 'A'
+        0              | 9                 || '+'
+        0              | 0                 || ' '
+        0              | 1                 || '@'
+        2              | 1                 || 'x'
     }
 
-    def "test getCharOnPosition_providedNoNEmptyList_charOnCurrentPositionIsNull"() {
+    @Unroll
+    def "test isPositionValid_labyrinthWithProvidedNoNEmptyList_positionIsValid=#expectedPositionValid"() {
         given:
         List<String> lines = FileUtils.readFile("ascii_path_1.txt")
         Labyrinth labyrinth = new Labyrinth(lines);
 
         when:
-        String charOnCurrentPosition = labyrinth.getCharOnPosition(Position.of(0, 5))
+        boolean isPositionValid = labyrinth.isPositionValid(Position.of(sourceRowIndex, sourceColumnIndex))
 
         then:
-        charOnCurrentPosition == 'A'
+        isPositionValid == expectedPositionValid
+
+        where:
+        sourceRowIndex | sourceColumnIndex || expectedPositionValid
+        -1             | 5                 || false
+        0              | 5                 || true
+        5              | 100               || false
+        3              | -1                || false
+    }
+
+    @Unroll
+    def "test isPositionValidAndIsNotEqualTo_labyrinthWithProvidedNoNEmptyList_positionIsValid=#expectedPositionValid"() {
+        given:
+        List<String> lines = FileUtils.readFile("ascii_path_1.txt")
+        Labyrinth labyrinth = new Labyrinth(lines);
+
+        when:
+        boolean isPositionValid = labyrinth.isPositionValidAndIsNotEqualTo(Position.of(sourceRowIndex, sourceColumnIndex), unallowdChars)
+
+        then:
+        isPositionValid == expectedPositionValid
+
+        where:
+        sourceRowIndex | sourceColumnIndex | unallowdChars          || expectedPositionValid
+        -1             | 5                 | new String[]{}         || false
+        0              | 5                 | new String[]{}         || true
+        5              | 100               | new String[]{}         || false
+        3              | -1                | new String[]{}         || false
+        0              | 2                 | new String[]{" ", "-"} || false
+        0              | 2                 | new String[]{" ", "+"} || true
+    }
+
+    @Unroll
+    def "test isDownPositionValidAndNotEqualTo_labyrinthWithProvidedNoNEmptyList_positionIsValid=#expectedPositionValid"() {
+        given:
+        List<String> lines = FileUtils.readFile("ascii_path_1.txt")
+        Labyrinth labyrinth = new Labyrinth(lines);
+
+        when:
+        boolean isPositionValid = labyrinth.isDownPositionValidAndNotEqualTo(unallowedChars)
+
+        then:
+        isPositionValid == expectedPositionValid
+
+        where:
+        unallowedChars    || expectedPositionValid
+        new String[]{}    || true
+        new String[]{" "} || false
+    }
+
+    @Unroll
+    def "test isUpperPositionValidAndNotEqualTo_labyrinthWithProvidedNoNEmptyList_positionIsValid=#expectedPositionValid"() {
+        given:
+        List<String> lines = FileUtils.readFile("ascii_path_1.txt")
+        Labyrinth labyrinth = new Labyrinth(lines);
+
+        when:
+        boolean isPositionValid = labyrinth.isUpperPositionValidAndNotEqualTo(unallowedChars)
+
+        then:
+        isPositionValid == expectedPositionValid
+
+        where:
+        unallowedChars    || expectedPositionValid
+        new String[]{}    || false
+        new String[]{" "} || false
+    }
+
+    @Unroll
+    def "test isLeftPositionValidAndNotEqualTo_labyrinthWithProvidedNoNEmptyList_positionIsValid=#expectedPositionValid"() {
+        given:
+        List<String> lines = FileUtils.readFile("ascii_path_1.txt")
+        Labyrinth labyrinth = new Labyrinth(lines);
+
+        when:
+        boolean isPositionValid = labyrinth.isLeftPositionValidAndNotEqualTo(unallowedChars)
+
+        then:
+        isPositionValid == expectedPositionValid
+
+        where:
+        unallowedChars    || expectedPositionValid
+        new String[]{}    || true
+        new String[]{" "} || false
+    }
+
+    @Unroll
+    def "test isRightPositionValidAndNotEqualTo_labyrinthWithProvidedNoNEmptyList_positionIsValid=#expectedPositionValid"() {
+        given:
+        List<String> lines = FileUtils.readFile("ascii_path_1.txt")
+        Labyrinth labyrinth = new Labyrinth(lines);
+
+        when:
+        boolean isPositionValid = labyrinth.isRightPositionValidAndNotEqualTo(unallowedChars)
+
+        then:
+        isPositionValid == expectedPositionValid
+
+        where:
+        unallowedChars    || expectedPositionValid
+        new String[]{}    || true
+        new String[]{" "} || true
+        new String[]{"-"} || false
+    }
+
+    @Unroll
+    def "test isCurrentPositionValidAndNotEqualTo_labyrinthWithProvidedNoNEmptyList_positionIsValid=#expectedPositionValid"() {
+        given:
+        List<String> lines = FileUtils.readFile("ascii_path_1.txt")
+        Labyrinth labyrinth = new Labyrinth(lines);
+
+        when:
+        boolean isPositionValid = labyrinth.isCurrentPositionValidAndNotEqualTo(unallowedChars)
+
+        then:
+        isPositionValid == expectedPositionValid
+
+        where:
+        unallowedChars    || expectedPositionValid
+        new String[]{}    || true
+        new String[]{" "} || true
+        new String[]{"-"} || true
+        new String[]{"@"} || false
     }
 }
