60c9df1029280b49fb6ef4d1ec9a9ce88f762d65
Martina Lucan <martina.lucan@adcubum.com>
2021-04-13 08:52:48 +0200

    removed unused methods

--- app/src/main/java/model/Direction.java
+++ app/src/main/java/model/Direction.java
@@ -5,21 +5,5 @@ public enum Direction {
    RIGHT,
    LEFT,
    UP,
-   DOWN;
-
-   public boolean isLeft() {
-      return this == LEFT;
-   }
-
-   public boolean isRight() {
-      return this == RIGHT;
-   }
-
-   public boolean isUp() {
-      return this == UP;
-   }
-
-   public boolean isDown() {
-      return this == DOWN;
-   }
+   DOWN
 }
